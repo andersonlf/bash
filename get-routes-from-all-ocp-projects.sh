e57641c46e10d5fccae7a55349ac90dbbccaf33a
Anderson Lobo Feitosa <andersonlf@gmail.com>
2020-04-26 12:28:45 -0300

    make it better

---
+++ get-routes-from-all-ocp-projects.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# obtem a lista de projetos com o nome curto, sem descricao
+for a in $(oc projects --short); do { 
+
+  if [ "$a" != "3scale" ];
+  then
+
+    # troca de projeto em silencio, por favor
+    oc project "$a" --short > /dev/null
+
+    # obtem a lista de todos os routes em formato json
+    # usa jq para varrer a lista de itens
+    # usa jq para obter apenas a url
+    URL=$(oc get route -o json | jq .items[] | jq .spec.host)
+
+    # imprime apenas se houve alguma url
+    # formalmente imprime se url nao eh null
+    if [ "$URL" ];
+    then
+        echo "$URL"
+    fi
+
+  fi
+
+}; done;
